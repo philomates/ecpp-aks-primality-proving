8311c08cead5cd49150e7fa7af71d810eb69196e
Phillip Mates <phillipmates@gmail.com>
2011-10-26 00:57:47 -0600

    New papers on AKS that I haven't read

--- naive_aks.hs
+++ naive_aks.hs
@@ -24,13 +24,29 @@ findR r n
 
   | otherwise = findR (r+1) n
 
+-- b^k (mod m)
+-- Via paper titled "Implementing Public-Key Cryptography in Haskell"
+expm :: Integer -> Integer -> Integer -> Integer
+expm m b k =
+  let
+    ex a k s
+      | k == 0 = s
+      | k `mod` 2 == 0 = ((ex (a*a `mod` m)) (k `div` 2)) s
+      | otherwise = ((ex (a*a `mod` m)) (k `div` 2)) (s*a `mod` m)
+  in ex b k 1
+
+-- naive slow b^k (mod m)
+expm' :: Integer -> Integer -> Integer -> Integer
+expm' m b k = (b ^ k) `mod` m
+
+
 -- TODO: primality check for smaller numbers
 isPrime :: Integer -> Bool
 isPrime = undefined
 
 fermatsCheck :: Integer -> Integer -> Integer -> Bool
 fermatsCheck a r n
-  | a < 2 * floor (sqrt (fromIntegral r)) * floor (logBase 2 (fromIntegral n)) =
+  | a < 2 * (floorSqrt r) * floorLn n =
     if a^n `mod` n == a -- FIXME: incorrect check!
        then False
        else fermatsCheck (a+1) r n
@@ -38,12 +54,18 @@ fermatsCheck a r n
 
 powerOfPrime :: Integer -> Integer -> Integer -> Bool
 powerOfPrime n a b
-  | and [a < floor (sqrt $ fromIntegral n)
-       , b > floor (logBase 2 (fromIntegral n))] = powerOfPrime n (a+1) 1
-  | and [a >= floor (sqrt $ fromIntegral n)
-       , b > floor (logBase 2 (fromIntegral n))] = False
+  | and [a < floorSqrt n
+       , b > flooLn n] = powerOfPrime n (a+1) 1
+  | and [a >= floorSqrt n
+       , b > flooLn n] = False
   | otherwise =
     case compare n (a^b) of
       EQ -> True
       LT -> powerOfPrime n a (b+1)
       GT -> powerOfPrime n (a+1) 2
+
+floorSqrt :: Integer -> Integer
+floorSqrt n = floor $ sqrt $ fromIntegral n
+
+floorLn :: Integer -> Integer
+floorLn n = floor $ logBase 2 (fromIntegral n)
